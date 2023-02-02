3fa3ad9a4d21b99a957827152ff288b6d70bff4f
Eva <evalienigena@gmail.com>
2023-02-01 23:35:38 -0600

    feat: add project structure + config files
    - add project structure
    - add config files for code style / linting
    - add dependencies for code style / linting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['google', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: '2021',
+  },
+  rules: {},
+};
